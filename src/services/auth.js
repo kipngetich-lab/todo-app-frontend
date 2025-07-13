879784817aecef64c54f7f48582749ad0e8b0b55
Kipngetich Josphat <joskipngetich07@gmail.com>
2025-07-14 00:01:47 +0300

    change api url

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,18 +1,25 @@
 import axios from 'axios'
 
-const API_URL = '/api/auth'
+//const API_URL = '/api/auth'
 //const API_URL = process.env.NODE_ENV === 'production' 
  // ? 'https://todo-app-backend-aqzv.onrender.com/api/auth' 
   //: 'http://localhost:5000/api/auth';
+const API_URL = import.meta.env.PROD
+  ? 'https://todo-app-bckend-aqzv.onrender.com/api'
+  : 'http://localhost:5000/api';
 
 const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/auth/register`, userData);
+  return response.data;
+};
+//const register = async (userData) => {
   //const response = await axios.post(`/api/auth/register`, userData)
-  const response = await axios.post(`${API_URL}/register`, userData)
-  return response.data
-}
+  //const response = await axios.post(`${API_URL}/register`, userData)
+ // return response.data
+//}
 
 const login = async (userData) => {
-  const response = await axios.post(`${API_URL}/login`, userData)
+  const response = await axios.post(`${API_URL}/auth/login`, userData)
   if (response.data.token) {
     localStorage.setItem('user', JSON.stringify(response.data))
   }
